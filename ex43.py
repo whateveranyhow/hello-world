b158d19c08effd024417840bc8cad3d9803f84d1
whateveranyhow <550811693@qq.com>
2017-02-04 16:24:33 +0800

    Update ex43.py

--- ex43.py
+++ ex43.py
@@ -1,3 +1,4 @@
+# while-and-if-else /guess
 from sys import exit
 
 def guess():
@@ -20,3 +21,35 @@ def guess():
       exit()
 
 guess()
+
+
+
+# if-elif-else /guess
+from sys import exit
+def guess():
+    print "give me a word, 1 will be bad and 1 good, else is back"
+
+    action = raw_input(">")
+
+    if action == "yes": #good
+        print "you win"
+        exit()
+
+    elif action == "no": #bad
+        print "you loss"
+        exit()
+
+    else:
+        print "back again"
+        return guess()
+
+guess()
+
+
+
+# randint
+from random import randint
+
+a = [1,2,3,5,3,6,2,8]
+#print len(a)
+print a[randint(0, len(a)-1)]
