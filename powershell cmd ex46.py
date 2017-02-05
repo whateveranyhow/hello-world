625eb7294b9fa7fd285a3a6b731524c041e3d716
whateveranyhow <550811693@qq.com>
2017-02-05 20:38:27 +0800

    Update and rename powershell cmd to powershell cmd ex46.py

---
+++ powershell cmd ex46.py
@@ -0,0 +1,14 @@
+new-item -type file tests/__init__.py
+# make a .py named __init__ in a file named tests.
+
+
+
+a skeleton has bin; 
+               NAME(__init__.py); 
+               tests(__init__.py and NAME_tests.py); 
+               docs;
+                setup.py
+
+.cpy
+before cd tests, enter nosetests(an installed whl file), will get .cpy.
+it's like a compliled file to fasten the running speed.
